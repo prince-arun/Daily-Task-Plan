75f5a2f88cad9672d18254ae1888a4fcaa0ee540
prince-arun <prince.skillsafari@gmail.com>
2023-12-22 21:53:43 +0530

    styling libraries configured

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,9 +1,30 @@
 import React from "react";
+import { Col, Button, Row, Container, Card } from "react-bootstrap";
 
 const Login = () => {
   return (
     <div className="login">
-      <h1>welcome to login page</h1>
+      <Container>
+        <Row className="vh-100 d-flex justify-content-center align-items-center">
+          <Col md={8} lg={6} xs={12}>
+            <div className="border border-3 border-primary"></div>
+            <Card className="shadow">
+              <Card.Body>
+                <div className="mb-3 mt-md-4">
+                  <h2 className="fw-bold mb-2 text-uppercase ">
+                    Daily Task Planner
+                  </h2>
+                  <p className=" mb-5">Login Using Google Account</p>
+                  <div className="mb-3">
+                    <Button variant="outline-primary">Google Sign In</Button>{" "}
+                    <div className="mt-3"></div>
+                  </div>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
